ff183863bcce5c09a25222f0bf1ba69db2601d14
John David Pressman <jd@jdpressman.com>
2024-07-18 23:15:59 -0700

    Add easy prose repair diffs, train/val split to Mix

--- Mix/dataloader.py
+++ Mix/dataloader.py
@@ -10,13 +10,42 @@ class RetroInstructDataloader:
             self.yes_no_prefixes = [line.strip() for line in file.readlines()]
         with open("art_prefixes.txt") as infile:
             self.art_prefixes = [line.strip() for line in infile.readlines()]
-        self.analogies = datasets.load_dataset("jdpressman/retro-weave-eval-analogical-translations-v0.1")["train"]
-        self.weave_eval_jdp = datasets.load_dataset("jdpressman/retro-weave-eval-jdp-v0.1")["train"]
-        self.weave_eval_rubrics = datasets.load_dataset("jdpressman/retro-weave-eval-rubrics-v0.1")["train"]
-        self.word_parts = datasets.load_dataset("jdpressman/retro-word-parts-v0.1")["train"]
-        self.ascii_art = datasets.load_dataset("jdpressman/retro-ascii-art-v1")["train"]
-        self.style_transfer = datasets.load_dataset("jdpressman/retro-text-style-transfer-v0.1")["train"]
-
+        self.analogies = (
+            [i for i in datasets.load_dataset("jdpressman/retro-weave-eval-analogical-translations-v0.1")["train"]]
+            + [i for i in datasets.load_dataset("jdpressman/retro-weave-eval-analogical-translations-v0.1")["validation"]]
+        )
+        random.shuffle(self.analogies)
+        self.weave_eval_jdp = (
+            [i for i in datasets.load_dataset("jdpressman/retro-weave-eval-jdp-v0.1")["train"]]
+            + [i for i in datasets.load_dataset("jdpressman/retro-weave-eval-jdp-v0.1")["validation"]]
+        )
+        random.shuffle(self.weave_eval_jdp)
+        self.weave_eval_rubrics = (
+            [i for i in datasets.load_dataset("jdpressman/retro-weave-eval-rubrics-v0.1")["train"]]
+            + [i for i in datasets.load_dataset("jdpressman/retro-weave-eval-rubrics-v0.1")["validation"]]
+        )
+        random.shuffle(self.weave_eval_rubrics)
+        self.word_parts = (
+            [i for i in datasets.load_dataset("jdpressman/retro-word-parts-v0.1")["train"]]
+            + [i for i in datasets.load_dataset("jdpressman/retro-word-parts-v0.1")["validation"]]
+        )
+        random.shuffle(self.word_parts)
+        self.ascii_art = (
+            [i for i in datasets.load_dataset("jdpressman/retro-ascii-art-v1")["train"]]
+            + [i for i in datasets.load_dataset("jdpressman/retro-ascii-art-v1")["validation"]]
+        )
+        random.shuffle(self.ascii_art)
+        self.style_transfer = (
+            [i for i in datasets.load_dataset("jdpressman/retro-text-style-transfer-v0.1")["train"]]
+            + [i for i in datasets.load_dataset("jdpressman/retro-text-style-transfer-v0.1")["validation"]]
+        )
+        random.shuffle(self.style_transfer)
+        self.easy_prose_diffs = (
+            [i for i in datasets.load_dataset("jdpressman/retro-easy-prose-repair-diffs-v0.1")["train"]]
+            + [i for i in datasets.load_dataset("jdpressman/retro-easy-prose-repair-diffs-v0.1")["validation"]]
+        )
+        random.shuffle(self.easy_prose_diffs)
+        
         self.epoch = []
         self.epoch += self.prepare_analogies()
         self.epoch += self.prepare_jdp()
@@ -24,6 +53,7 @@ class RetroInstructDataloader:
         self.epoch += self.prepare_word_parts()
         self.epoch += self.prepare_eval_rubrics()
         self.epoch += self.prepare_style_transfer()
+        self.epoch += self.prepare_easy_prose_diffs()
 
         random.shuffle(self.epoch)
 
@@ -62,7 +92,7 @@ class RetroInstructDataloader:
                             + "Prior Arguments:\n" + priors + "\n\n"
                             + "Chosen Argument: " + entry["chosen-argument"] + "\n\n"
                             + "Differences:\n" + differences + "\n\n")
-                entries.append(mistral_instruction.format(question, response))
+                entries.append({"inputs":question, "targets":response})
             else:
                 question = (entry["analogical-translation"] + "\n\n"
                             + "Criticism:\n" + footnotes + "\n\n"
@@ -75,7 +105,7 @@ class RetroInstructDataloader:
                             + "Differences:\n" + differences + "\n\n"
                             + f"{good_faith}")
                 response = entry["label"]
-                entries.append(mistral_instruction.format(question, response))
+                entries.append({"inputs":question, "targets":response})
         return entries
 
     def prepare_jdp(self):
@@ -103,7 +133,7 @@ class RetroInstructDataloader:
                     instruction += f"{question_text}"
                     output += f"{label}. {explanation}"
 
-            entries.append(mistral_instruction.format(instruction, output))
+            entries.append({"inputs":instruction, "targets":output})
         return entries
     
     def prepare_ascii_art(self):
@@ -118,21 +148,22 @@ class RetroInstructDataloader:
         for ascii_art, mode in epoch:
             roll = random.randrange(5)
             if roll == 0:
-                out = f"<s> [INST]{ascii_art['prompt']}[/INST]"
+                inputs = ascii_art['prompt']
             else:
                 prefix = random.choice(self.art_prefixes)
-                out = f"<s> [INST]{prefix} {ascii_art['prompt'].lower()}[/INST]"
+                inputs = f"{prefix} {ascii_art['prompt'].lower()}"
             if mode == "aic":
-                out += ascii_art['art_aic']
+                art = ascii_art['art_aic']
             elif mode == "i2a":
-                out += ascii_art['art_i2a']
-            entries.append(out)
+                art = ascii_art['art_i2a']
+            entries.append({"inputs":inputs, "targets":art})
         return entries
 
     def prepare_word_parts(self):
         word_filter = {'ovgpu', 'phag', 'snttbgf', 'snttbg', 'snt', 'fuvg', 'cvff'}
         entries = []
-        for row in self.word_parts:
+        # These are overrepresented if we don't cap how many we use
+        for row in self.word_parts[:5000]:
             if codecs.encode(row["word"], 'rot_13') in word_filter:
                 continue
             # Note: I suggest formatting the dataset into multiple structures such as JSON
@@ -142,17 +173,16 @@ class RetroInstructDataloader:
             # instruction models to fluidly work with both user prompts and structured
             # prompts more fitting for automated pipelines.
             # Concept Synthesis
-            entries.append("<s> [INST]"
-                           + row["synth_prompt"]
+            entries.append(
+                {"inputs":(row["synth_prompt"]
                            + "\n\n"
-                           + row["part_list"]
-                           + f"[/INST]1. {row['word'].capitalize()} - "
-                           + row["word_guesses"] if row["word_guesses"] else "")
+                           + row["part_list"]),
+                 "targets":(f"1. {row['word'].capitalize()} - "
+                            + row["word_guesses"] if row["word_guesses"] else "")}
+            )
             # Word Parts
-            entries.append("<s> [INST]"
-                           + row["list_prompt"].format(WORD=row["word"])
-                           + "[/INST]"
-                           + row["part_list"])
+            entries.append({"inputs":row["list_prompt"].format(WORD=row["word"]),
+                            "targets":row["part_list"]})
         return entries
 
     def prepare_eval_rubrics(self):
@@ -165,7 +195,7 @@ class RetroInstructDataloader:
             for item in row["rubric"]:
                 output += (str(index) + ". " + item + "\n")
                 index += 1
-            entries.append(mistral_instruction.format(prompt, output))
+            entries.append({"inputs":prompt, "targets":output})
         return entries
 
     def prepare_style_transfer(self):
@@ -180,21 +210,20 @@ class RetroInstructDataloader:
                 continue
             try:
                 entries.append(
-                    ''.join([row["prompt_open"],
-                             "\n\n",
-                             row["start_style"],
-                             "\n",
-                             row["style_passage"],
-                             "\n",
-                             row["end_style"],
-                             "\n\n",
-                             row["start_task"],
-                             "\n",
-                             row["task_passage"],
-                             "\n",
-                             row["end_task"],
-                             "\n\n",
-                             row["ground_truth"]])
+                    {"inputs":''.join([row["prompt_open"],
+                                       "\n\n",
+                                       row["start_style"],
+                                       "\n",
+                                       row["style_passage"],
+                                       "\n",
+                                       row["end_style"],
+                                       "\n\n",
+                                       row["start_task"],
+                                       "\n",
+                                       row["task_passage"],
+                                       "\n",
+                                       row["end_task"]]),
+                     "targets":row["ground_truth"]}
                 )
             # No seriously this dataset needs schema checking
             except TypeError:
@@ -202,7 +231,58 @@ class RetroInstructDataloader:
                 continue
         return entries
 
+    def prepare_easy_prose_diffs(self):
+        mistral_instruction = "<s> [INST]{}[/INST]{}"
+        entries = []
+        diff_formats = ["gnudiff", "git", "dmp"]
+
+        for diff in self.easy_prose_diffs:
+            diff_format = random.choice(diff_formats)
+            if diff_format == "gnudiff":
+                instruction = diff["gnudiff_instruction"]
+                diff_text = diff["gnudiff"]
+            elif diff_format == "git":
+                instruction = diff["gitdiff_instruction"]
+                diff_text = diff["gitdiff"]
+            elif diff_format == "dmp":
+                instruction = diff["dmpdiff_instruction"]
+                diff_text = diff["dmpdiff"]
+            entries.append(
+                {"inputs":''.join([
+                    instruction,
+                    "\n\n",
+                    "<passage>\n",
+                    diff["text_corrupted"],
+                    "\n</passage>",
+                    "<|end|>"]),
+                 "targets":''.join([
+                     "<diagnosis>\n",
+                     diff["operations"],
+                     "</diagnosis>\n",
+                     "<diff>\n",
+                     diff_text,
+                     "</diff>\n",
+                     "<repaired>\n",
+                     diff["text_clean"],
+                     "</repaired>"])
+                }
+            )
+        return entries
+
+    
 dataloader = RetroInstructDataloader()
 
+dataset = [entry for entry in dataloader]
+
+val_start = len(dataset) - round(len(dataset) / 10)
+
+with open("train.json", "w") as outfile:
+    json.dump(dataset[:val_start], outfile)
+
+with open("val.json", "w") as outfile:
+    json.dump(dataset[val_start:], outfile)
+    
 for entry in dataloader:
+    assert type(entry["inputs"]) == str
+    assert type(entry["targets"]) == str
     print(entry)
